3c6a4fac440c89fc56358e86ef46ee682ba48b1f
David Clarkson <clarkee013@gmail.com>
2017-08-02 23:24:11 +0100

    changed from list view to drop down, background image added

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 var app = function(){
     var url = "https://api.punkapi.com/v2/beers"
     makeRequest(url, requestComplete)
+    
 }
 
 var makeRequest = function(url, callback){
@@ -14,28 +15,56 @@ var requestComplete = function(){
     if(this.status !== 200) return;
     var jsonString = this.responseText;
     var beers = JSON.parse(jsonString);
-    listBeersOnPage(beers);
+    populateDropDown(beers);
+    // listBeersOnPage(beers);
     // showPicturesOfBeers(beers); 
 }
 
-var listBeersOnPage = function (beers) {
-    var ul = document.getElementById('beer-list');
-    beers.forEach(function (beer) {
-        var li = document.createElement('li');
-        li.innerText = beer.name;
-        var image = document.createElement('img');
-        image.src = beer.image_url;
-        image.style.height = "200px";
-        var tagLine = document.createElement('tagLine');
-        tagLine.innerText = beer.tagline;
-        var blurb = document.createElement('blurb');
-        blurb.innerText = beer.description;
-        ul.appendChild(li);
-        ul.appendChild(image);
-        ul.appendChild(tagLine);
-        ul.appendChild(blurb);
+// var listBeersOnPage = function (beers) {
+//     var li = document.getElementById('beer-list');
+//     beers.forEach(function (beer) {
+//         var liA = document.createElement('li');
+//         liA.innerText = beer.name;
+//         var image = document.createElement('img');
+//         image.src = beer.image_url;
+//         image.style.height = "200px";
+//         var tagLine = document.createElement('tagLine');
+//         tagLine.innerText = beer.tagline;
+//         var blurb = document.createElement('blurb');
+//         blurb.innerText = beer.description;
+//         li.appendChild(liA);
+//         li.appendChild(image);
+//         li.appendChild(tagLine);
+//         li.appendChild(blurb);
+//     })
+// }
+
+var populateDropDown = function (beers) {
+    var select = document.getElementById('beerDropDown');
+    beers.forEach(function (beer, index) {
+        var option = document.createElement('option');
+        option.innerText = beer.name;
+        option.value = index;
+        select.appendChild(option);
     })
+    select.addEventListener("change", function(){
+        var index = select.value;
+        var beer = beers[index];
+        displayBeerDetails(beer);
+     
+    });
+
+    var displayBeerDetails = function (beer) {
+    var name = document.getElementById('beer-name');
+    var tagline = document.getElementById('tagline');
+    var description = document.getElementById('description');
+    name.innerText = beer.name;
+    tagline.innerText = beer.tagline;
+    description.innerText = beer.description;
 }
+}
+
+
 
 
 
